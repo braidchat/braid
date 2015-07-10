7c0ede53aedeaaf21cab88b7630f4c0d6168feb1
Rafal Dittwald <rafal.dittwald@gmail.com>
2015-07-10 15:44:41 -0400

    threads & replies

--- src/chat/client/core.cljs
+++ src/chat/client/core.cljs
@@ -7,13 +7,27 @@
 
 (def app-state (atom {:messages []}))
 
+(defn guid []
+  (rand 100000))
+
 (defn transact! [ks f]
   (swap! app-state update-in ks f))
 
+(defn seed! []
+  (transact! [:messages] (constantly [{:content "Hello?" :thread-id 123 :user-id 1}
+                                     {:content "Hi!" :thread-id 123 :user-id 2}
+                                     {:content "Oh, great, someone else is here." :thread-id 123 :user-id 1}
+                                     {:content "Yep" :thread-id 123 :user-id 2}])))
+
 (defmulti dispatch! (fn [event data] event))
 
-(defmethod dispatch! :new-message [_ data]
-  (transact! [:messages] #(conj % {:content data})))
+(defmethod dispatch! :new-thread-message [_ data]
+  (transact! [:messages] #(conj % {:content data
+                                   :thread-id (guid)})))
+
+(defmethod dispatch! :reply-thread-message [_ data]
+  (transact! [:messages] #(conj % {:content (data :content)
+                                   :thread-id (data :thread-id)})))
 
 (defn message-view [message owner]
   (reify
@@ -23,28 +37,55 @@
         (dom/div #js {:className "content"}
           (message :content))))))
 
-(defn new-message-view [data owner]
+(defn new-reply-view [thread owner]
   (reify
     om/IRender
     (render [_]
       (dom/div #js {:className "message new"}
-        (dom/textarea #js {:placeholder "Start a new conversation..."
+        (dom/textarea #js {:placeholder "Reply..."
                            :onKeyDown (fn [e]
                                         (when (and (= 13 e.keyCode) (= e.shiftKey false))
-                                          (dispatch! :new-message (.. e -target -value))
+                                          (dispatch! :reply-thread-message {:thread-id (thread :id)
+                                                                            :content (.. e -target -value)} )
                                           (.preventDefault e)
                                           (aset (.. e -target) "value" "")))})))))
+(defn thread-view [thread owner]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/div #js {:className "thread"}
+        (apply dom/div #js {:className "messages"}
+          (om/build-all message-view (thread :messages)))
+        (om/build new-reply-view thread)))))
+
+(defn new-thread-view [data owner]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/div #js {:className "thread"}
+        (dom/div #js {:className "message new"}
+          (dom/textarea #js {:placeholder "Start a new conversation..."
+                           :onKeyDown (fn [e]
+                                        (when (and (= 13 e.keyCode) (= e.shiftKey false))
+                                          (dispatch! :new-thread-message (.. e -target -value))
+                                          (.preventDefault e)
+                                          (aset (.. e -target) "value" "")))}))))))
 
 (defn app-view [data owner]
   (reify
     om/IRender
     (render [_]
-      (dom/div nil
-        (apply dom/div nil
-          (om/build-all message-view (data :messages)))
-        (om/build new-message-view {})))))
+      (let [threads (->> (data :messages)
+                         (group-by :thread-id)
+                         (map (fn [[id ms]] {:id id
+                                             :messages ms})))]
+        (dom/div nil
+          (apply dom/div nil
+            (om/build-all thread-view threads))
+          (om/build new-thread-view {}))))))
 
 
 (defn init []
   (om/root app-view app-state
-           {:target (. js/document (getElementById "app"))}))
+           {:target (. js/document (getElementById "app"))})
+  (seed!))
