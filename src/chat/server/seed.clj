d376c2721dbc217fe1f14d9ea915d682920df8db
Rafal Dittwald <rafal.dittwald@gmail.com>
2015-08-06 11:28:36 -0400

    fix seed to work with groups

--- src/chat/server/seed.clj
+++ src/chat/server/seed.clj
@@ -10,7 +10,11 @@
 
 (defn seed! []
   (db/with-conn
-    (let [user-1 (db/create-user! {:id (db/uuid)
+    (let [group-1 (db/create-group! {:id (db/uuid)
+                                     :name "Lean Pixel"})
+          group-2 (db/create-group! {:id (db/uuid)
+                                     :name "Penyo Pal"})
+          user-1 (db/create-user! {:id (db/uuid)
                                    :email "raf@leanpixel.com"
                                    :password (get env :rafal-password "foobar")
                                    :avatar "https://s3.amazonaws.com/chat.leanpixel.com/avatars/4801271456_41230ac0b881cdf85c28_72.jpg"})
@@ -18,13 +22,20 @@
                                    :email "james@leanpixel.com"
                                    :password (get env :james-password "foobar")
                                    :avatar "https://s3.amazonaws.com/chat.leanpixel.com/avatars/4805955000_07dc272f0a7f9de7719e_192.jpg"})
+          _ (db/user-add-to-group! (user-1 :id) (group-1 :id))
+          _ (db/user-add-to-group! (user-1 :id) (group-2 :id))
+          _ (db/user-add-to-group! (user-2 :id) (group-1 :id))
+          _ (db/user-add-to-group! (user-2 :id) (group-2 :id))
           user-3 (db/create-user! {:id (db/uuid)
                                    :email "test@ycombinator.com"
                                    :password (get env :tester-password "")
                                    :avatar "https://s3.amazonaws.com/chat.leanpixel.com/ycombinator-logo.png"})
 
-          tag-1 (db/create-tag! {:id (db/uuid) :name "penyopal"})
-          tag-2 (db/create-tag! {:id (db/uuid) :name "watercooler"})
+          tag-1 (db/create-tag! {:id (db/uuid) :group-id (group-1 :id) :name "idonethis"})
+          tag-2 (db/create-tag! {:id (db/uuid) :group-id (group-1 :id) :name "watercooler"})
+
+          tag-3 (db/create-tag! {:id (db/uuid) :group-id (group-2 :id) :name "idonethis"})
+          tag-4 (db/create-tag! {:id (db/uuid) :group-id (group-2 :id) :name "watercooler"})
 
           _ (db/user-subscribe-to-tag! (user-1 :id) (tag-1 :id))
           _ (db/user-subscribe-to-tag! (user-2 :id) (tag-1 :id))
