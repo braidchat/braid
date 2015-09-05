b34add4c2925f9f51d7c67a043ea14d765a0bc2f
James N. V. Cash <james.nvc@gmail.com>
2015-09-05 13:53:20 -0400

    wrap auth & sync routes in api-defaults

--- src/chat/server/handler.clj
+++ src/chat/server/handler.clj
@@ -3,7 +3,7 @@
   (:require [org.httpkit.server :refer [run-server]]
             [compojure.route :refer [resources]]
             [compojure.core :refer [GET POST routes defroutes context]]
-            [ring.middleware.defaults :refer [wrap-defaults secure-site-defaults site-defaults]]
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults secure-site-defaults site-defaults]]
             [ring.middleware.edn :refer [wrap-edn-params]]
             [clojure.string :as string]
             [clojure.tools.nrepl.server :as nrepl]
@@ -32,14 +32,10 @@
     (let [fail {:status 400 :headers {"Content-Type" "text/plain"}}]
       (cond
         (string/blank? password) (assoc fail :body "Must provide a password")
-
         (not (invites/verify-hmac hmac (str now token invite_id email))) (assoc fail :body "Invalid HMAC")
-
         (string/blank? invite_id) (assoc fail :body "Invalid invitation ID")
-
         ; TODO: be smarter about this
         (not (#{"image/jpeg" "image/png"} (:content-type avatar))) (assoc fail :body "Invalid image")
-
 
         :else
         (let [invite (db/with-conn (db/get-invite (java.util.UUID/fromString invite_id)))]
@@ -58,14 +54,16 @@
                :session (assoc (req :session) :user-id (user :id))
                :body ""}))))))
 
+  (POST "/logout" req
+    {:status 200 :session nil}))
+
+(defroutes api-routes
   (POST "/auth" req
     (if-let [user-id (let [{:keys [email password]} (req :params)]
                        (when (and email password)
                          (db/with-conn (db/authenticate-user email password))))]
       {:status 200 :session (assoc (req :session) :user-id user-id)}
-      {:status 401}))
-  (POST "/logout" req
-    {:status 200 :session nil}))
+      {:status 401})))
 
 (defroutes resource-routes
   (resources "/"))
@@ -75,20 +73,27 @@
                                     :port 6379}})
 
 (def app
-  (->
+  (routes
     (wrap-defaults
-      (routes
-        resource-routes
-        site-routes
-        sync-routes)
-      (-> site-defaults ; ssl stuff will be handled by nginx
+      (routes sync-routes api-routes)
+      (-> api-defaults
           (assoc-in [:session :cookie-attrs :secure] (= (env :environment) "prod"))
           (assoc-in [:session :store] (carmine/carmine-store *redis-conf*
                                                              {:expiration-secs (* 60 60 24 7)
-                                                              :key-prefix "lpchat"}))
-          (assoc-in [:security :anti-forgery]
-            {:read-token (fn [req] (-> req :params :csrf-token))})))
-      wrap-edn-params))
+                                                              :key-prefix "lpchat"}))))
+      (->
+        (wrap-defaults
+          (routes
+            resource-routes
+            site-routes)
+          (-> site-defaults ; ssl stuff will be handled by nginx
+              (assoc-in [:session :cookie-attrs :secure] (= (env :environment) "prod"))
+              (assoc-in [:session :store] (carmine/carmine-store *redis-conf*
+                                                                 {:expiration-secs (* 60 60 24 7)
+                                                                  :key-prefix "lpchat"}))
+              (assoc-in [:security :anti-forgery]
+                {:read-token (fn [req] (-> req :params :csrf-token))})))
+        wrap-edn-params)))
 
 (defonce server (atom nil))
 
