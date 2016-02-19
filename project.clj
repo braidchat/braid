780777aef1cd5870f70cde2157969d63eb6147c8
James N. V. Cash <james.nvc@gmail.com>
2016-02-19 15:17:53 -0500

    updating dependencies to try to fix deploy issues

--- project.clj
+++ project.clj
@@ -10,9 +10,7 @@
                  [fogus/ring-edn "0.3.0"]
                  [compojure "1.4.0"]
                  [environ "1.0.0"]
-                 [com.taoensso/sente "1.7.0"]
-                 [com.taoensso/timbre "4.0.2"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [com.taoensso/timbre "4.1.4"]
                  [crypto-password "0.1.3"]
                  [joda-time "2.8.2"]
                  [instaparse "1.4.1"]
@@ -22,17 +20,18 @@
                  [org.clojure/clojurescript "1.7.145"]
                  [org.omcljs/om "0.8.8"]
                  [org.clojars.leanpixel/cljs-utils "0.2.1"]
-                 [prismatic/schema "0.4.3"]
                  [secretary "1.2.3"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [com.lucasbradstreet/cljs-uuid-utils "1.0.2"]
                  [org.clojars.leanpixel/om-fields "1.8.6"]
-                 [com.andrewmcveigh/cljs-time "0.4.0"]]
+                 [com.andrewmcveigh/cljs-time "0.4.0"]
+                 ;shared
+                 [org.clojure/core.async "0.2.374" :exclusions [org.clojure/tools.reader]]
+                 [com.taoensso/sente "1.7.0"]]
 
   :main chat.server.handler
   :plugins [[lein-environ "1.0.0"]
             [lein-cljsbuild "1.0.6"]
-            [jamesnvc/lein-lesscss "1.4.0"] ]
+            [jamesnvc/lein-lesscss "1.4.0"]]
 
   :repl-options {:timeout 120000}
   :clean-targets ^{:protect false}
@@ -54,7 +53,9 @@
   :min-lein-version "2.5.0"
 
   :profiles {:dev {:dependencies [[com.datomic/datomic-free "0.9.5201" :exclusions [joda-time com.amazonaws/aws-java-sdk]]
-                                  [figwheel-sidecar "0.5.0-6"]]}
+                                  [figwheel-sidecar "0.5.0-6" :exclusions
+                                   [org.clojure/google-closure-library-third-party
+                                    com.google.javascript/closure-compiler]]]}
              :prod {:dependencies [[com.datomic/datomic-pro "0.9.5201" :exclusions [joda-time com.amazonaws/aws-java-sdk]]
                                    [org.postgresql/postgresql "9.3-1103-jdbc4"]
                                    [org.clojure/tools.nrepl "0.2.10"]]}
