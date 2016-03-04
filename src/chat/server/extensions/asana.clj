d225a0f0b3eb10ab5eaf897d3d10f16f37d7b78d
James N. V. Cash <james.nvc@gmail.com>
2016-03-04 14:22:45 -0500

    make comments update bidirectionally

--- src/chat/server/extensions/asana.clj
+++ src/chat/server/extensions/asana.clj
@@ -18,6 +18,11 @@
 (def client-id (env :asana-client-id))
 (def client-secret (env :asana-client-secret))
 
+; TODO: make these configurable?
+(def comment-format-str "Comment via Braid Chat from %s:\n%s")
+(def new-issue-format "New issues from %s:\n%s\n%s")
+(def issue-comment-format "New comment from %s:\n%s")
+
 ;; Setup
 
 (defn create-asana-extension
@@ -155,44 +160,61 @@
 
 (defn update-threads-from-event
   [extension data]
-  (let [extant-issues (set (keys (get-in extension [:config :issue->thread] {})))
+  (let [extant-issues (-> extension (get-in [:config :thread->issue] {}) vals set)
         new-issues (into []
                          (filter #(and (= "task" (% "type"))
                                        (= "added" (% "action"))))
                          (data "events"))
         new-comments (into []
-                           (filter #(and (= "task" (% "story"))
-                                         (= "changed" (% "added"))
+                           (filter #(and (= "story" (% "type"))
+                                         (= "added" (% "action"))
                                          (extant-issues (% "parent"))))
                            (data "events"))]
-    ; TODO: start a new thread for the issue & watch the thread
+    (timbre/debugf "%s new issues" (count new-issues))
     (doseq [{:strs [resource] :as issue} new-issues]
       (let [thread-id (db/uuid)
             task (fetch-asana-info (extension :id) (str "/tasks/" resource))
-            thread->issue (get-in extension [:config :thread->issue] {})]
+            thread->issue (get-in extension [:config :thread->issue] {})
+            tag-id (get-in extension [:config :tag-id])]
         (if-let [task-data (get task "data")]
           (do (timbre/debugf "adding thread for task %s" task-data)
               (db/with-conn
                 (db/create-message! {:thread-id thread-id
                                      :id (db/uuid)
-                                     :content (format "New issues from %s:\n%s"
-                                                      (get-in task-data ["followers" 0])
-                                                      (get task-data "name"))
+                                     :content (format new-issue-format
+                                                      (get-in task-data ["followers" 0 "name"])
+                                                      (task-data "name")
+                                                      (task-data "notes"))
                                      :user-id (extension :user-id)
                                      :created-at (java.util.Date.)
                                      :mentioned-user-ids ()
-                                     :mentioned-tag-ids ()})
+                                     :mentioned-tag-ids [tag-id]})
                 (db/set-extension-config!
-                  extension
-                  :thread->issue (assoc thread->issue thread-id resource)))
+                  (extension :id)
+                  :thread->issue (assoc thread->issue thread-id resource))
+                (db/extension-subscribe (extension :id) thread-id))
               (sync/broadcast-thread thread-id ()))
           (timbre/warnf "No such task %s" resource))))
     ; TODO: handle changed issue to add a new message to the thread
-    (let [issue->thread (-> (get-in extension [:config :thread->issue] {})
-                            (into {} (map (fn [t i] [i t]))))]
+    (timbre/debugf "%s new comments" (count new-comments))
+    (let [issue->thread (->> (get-in extension [:config :thread->issue] {})
+                             (into {} (map (fn [[t i]] [i t]))))]
       (doseq [{:strs [resource parent] :as story} new-comments]
         (if-let [thread-id (issue->thread parent)]
-          (do (timbre/debugf "new comment %s" story))
+          (let [story-data (-> (fetch-asana-info (extension :id) (str "/stories/" resource))
+                               (get "data"))]
+            (timbre/debugf "new comment %s" story story-data)
+            (db/with-conn
+              (db/create-message! {:thread-id thread-id
+                                   :id (db/uuid)
+                                   :content (format issue-comment-format
+                                              (get-in story-data ["created_by" "name"])
+                                              (story-data "text"))
+                                   :user-id (extension :user-id)
+                                   :created-at (java.util.Date.)
+                                   :mentioned-user-ids ()
+                                   :mentioned-tag-ids ()}))
+            (sync/broadcast-thread thread-id ()))
           (timbre/warnf "No existing thread for resource %s" resource))))))
 
 (defmethod handle-webhook :asana
@@ -203,14 +225,15 @@
           (db/set-extension-config! (extension :id) :webhook-secret secret))
       {:status 200 :headers {"X-Hook-Secret" secret}})
     (if-let [signature (get-in event-req [:headers "x-hook-signature"])]
-      (let [body (str (:body event-req))]
+      (let [body (:body event-req)
+            body (if (string? body) body (slurp body))]
         (timbre/debugf "webhook %s" event-req)
         (assert (some? (get-in extension [:config :webhook-secret]))
-          "Extension must have a webhook secret before it can recieve data")
+                "Extension must have a webhook secret before it can recieve data")
         (if (hmac-verify {:secret (get-in extension [:config :webhook-secret])
                           :data body
                           :mac signature})
-          (let [data (json/read-str (:body event-req))]
+          (let [data (json/read-str body)]
             (timbre/debugf "event data: %s" data)
             (update-threads-from-event extension data)
             {:status 200})
@@ -220,10 +243,6 @@
           {:status 400 :body "missing signature"}))))
 
 ;; watched thread notification
-
-(def comment-format-str
-  ; TODO: make this configurable?
-  "Comment via Braid Chat from %s:\n%s")
 
 (defmethod handle-thread-change :asana
   [extension msg]
