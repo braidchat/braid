32e9dbe0395176c8c66f930c652cc69ae93e4f2d
James N. V. Cash <james.nvc@gmail.com>
2016-03-19 17:54:52 -0400

    WIP: find updates to send to users for digest

--- src/chat/server/email_digest.clj
+++ src/chat/server/email_digest.clj
@@ -3,15 +3,40 @@
              [triggers :as t]
              [jobs :as j :refer [defjob]]]
             [clojurewerkz.quartzite.schedule.cron :as cron]
+            [clj-time
+             [core :as time]
+             [coerce :refer [to-date-time]]]
             [environ.core :refer [env]]
             [org.httpkit.client :as http]
             [taoensso.timbre :as timbre]
             [chat.server.db :as db]))
 
-(defn update-for-user
+; finding data
+
+(defn thread-unseen?
+  "A thread is unseen if it's last-open-at is prior to any of the messages' created-at"
+  [thread]
+  (let [last-open (to-date-time (thread :last-open-at))]
+    (some (fn [{:keys [created-at]}]
+            (time/before? last-open (to-date-time created-at)))
+          (thread :messages))))
+
+(defn updates-for-user
   [user-id]
   (db/with-conn
-    (db/get-open-threads-for-user user-id)))
+    (doall (filter thread-unseen? (db/get-open-threads-for-user user-id)))))
+
+; sending
+
+(defn send-message
+  [to {:keys [text html]}]
+  (http/post (str "https://api.mailgun.net/v3/" (env :mailgun-domain) "/messages")
+             {:basic-auth ["api" (env :mailgun-password)]
+              :form-params {:to to
+                            :from (str "noreply@" (env :mailgun-domain))
+                            :subject "While you were away"
+                            :text text
+                            :html html}}))
 
 ; Scheduling
 
