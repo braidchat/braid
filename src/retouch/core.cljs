a529a6908327a78f67777847c3ce9a899c96ff7a
Rafal Dittwald <rafal.dittwald@gmail.com>
2016-04-02 17:52:34 -0400

    move braid.mobile.sidebar -> retouch.views.drawer and braid.mobile.panels -> retouch.views.swipe

---
+++ src/retouch/core.cljs
@@ -0,0 +1,9 @@
+(ns retouch.core
+  (:require [retouch.views.swipe]
+            [retouch.views.drawer]))
+
+(def swipe-view (retouch.views.swipe/swipe-view))
+
+(def drawer-view (retouch.views.drawer/drawer-view))
+
+
