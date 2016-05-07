4bd7419a4124e46fe7c8b0c18480407bb7784dc5
James N. V. Cash <james.nvc@gmail.com>
2016-05-07 16:13:38 -0400

    fix group create form

--- src/braid/ui/views/pages/group_explore.cljs
+++ src/braid/ui/views/pages/group_explore.cljs
@@ -1,22 +1,30 @@
 (ns braid.ui.views.pages.group-explore
-  (:require [chat.client.dispatcher :refer [dispatch!]])
+  (:require [reagent.core :as r]
+            [chat.client.dispatcher :refer [dispatch!]])
   (:import [goog.events KeyCodes]))
 
 (defn group-explore-page-view
   []
-  [:div.page.group-explore
-   [:div.title "Group Explore"]
-   [:div.content
-    [:p "One day, you'll be able to see various open groups that may be worth exploring here."]
+  (let [new-group-name (r/atom "")]
+    (fn []
+      [:div.page.group-explore
+       [:div.title "Group Explore"]
+       [:div.content
+        [:p "One day, you'll be able to see various open groups that may be worth exploring here."]
 
-    [:h2 "Create Group"]
-    [:div.new-group
-     [:label "New Group"
-      [:input {:placeholder "Group Name"
-               :on-keydown
-               (fn [e]
-                 (when (= KeyCodes.ENTER e.keyCode)
-                   (.preventDefault e)
-                   (let [group-name (.. e -target -value)]
-                     (dispatch! :create-group {:name group-name})
-                     (set! (.. e -target -value) ""))))}]]]]])
+        [:h2 "Create Group"]
+        [:div.new-group
+         [:label "New Group"
+          [:input {:placeholder "Group Name"
+                   :value @new-group-name
+                   :on-change (fn [e] (reset! new-group-name (.. e -target -value)))
+                   :on-keydown
+                   (fn [e]
+                     (when (= KeyCodes.ENTER e.keyCode)
+                       (.preventDefault e)
+                       (dispatch! :create-group {:name @new-group-name})
+                       (reset! new-group-name "")))}]
+          [:button {:on-click (fn [_]
+                                (dispatch! :create-group {:name @new-group-name})
+                                (reset! new-group-name ""))}
+           "Create"]]]]])))
