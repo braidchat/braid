9e3f6609ce46b0556629b75fbb52480eb81dbe02
James N. V. Cash <james.nvc@gmail.com>
2016-05-10 10:26:58 -0400

    add link to issues in help page

--- src/braid/ui/views/pages/help.cljs
+++ src/braid/ui/views/pages/help.cljs
@@ -7,4 +7,7 @@
 
    [:div.content
     [:div.description
-     [:p "One day, a help page will be here."]]]])
+     [:p "One day, a help page will be here."]
+     [:p "If something is broken or confusing, please let us know by "
+      [:a {:href "https://github.com/braidchat/meta/issues" :target "_blank" :rel "noopener noreferrer"}
+       "Opening an issue"]]]]])
