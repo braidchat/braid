ffd21a4c4ff719a6bff8677d206e78127f4288a7
James N. V. Cash <james.nvc@gmail.com>
2016-05-09 22:13:35 -0400

    formatting

--- src/chat/client/schema.cljs
+++ src/chat/client/schema.cljs
@@ -9,8 +9,7 @@
    :user-id (or (data :user-id) nil)
    :created-at (or (data :created-at) (js/Date.))
    :mentioned-user-ids (or (data :mentioned-user-ids) [])
-   :mentioned-tag-ids (or (data :mentioned-tag-ids) [])
-   })
+   :mentioned-tag-ids (or (data :mentioned-tag-ids) [])})
 
 (defn make-tag [data]
   {:id (or (data :id) (uuid/make-random-squuid))
