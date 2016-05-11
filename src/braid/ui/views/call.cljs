951ccef12ded25a18cb02e745923e4f9af577b32
10plusY <grigsmusic1990@gmail.com>
2016-05-11 18:51:34 -0400

    made namespace for web rtc call views

---
+++ src/braid/ui/views/call.cljs
@@ -0,0 +1,9 @@
+(ns braid.ui.views.call
+  (:require [reagent.core :as r]
+            [reagent.impl.util :refer [extract-props]]))
+
+(defn caller-tag-view [])
+
+(defn calling-panel-view [])
+
+(defn call-interface-view [])
