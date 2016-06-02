da0488ba1bb33073b6bd1b505fe8011d2876d17c
sjuraschka <samjuraschka@gmail.com>
2016-06-02 11:29:42 -0400

    Update font styles to include similar fallback fonts

--- src/braid/ui/styles/imports.cljs
+++ src/braid/ui/styles/imports.cljs
@@ -3,4 +3,4 @@
 
 (def imports
   [(at-import      "https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css")
-  (at-import "https://fonts.googleapis.com/css?family=Open+Sans:400,600")])
+  (at-import "https://fonts.googleapis.com/css?family=Open+Sans:400,300,400italic,700")])
