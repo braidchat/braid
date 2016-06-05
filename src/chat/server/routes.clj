c144a07f66f1c814481da3d0e11b082719ed2fd5
James N. V. Cash <james.nvc@gmail.com>
2016-06-05 10:31:13 -0400

    move client routes into seperate ns

--- src/chat/server/routes.clj
+++ src/chat/server/routes.clj
@@ -1,13 +1,10 @@
 (ns chat.server.routes
   (:require
     [clojure.string :as string]
-    [clojure.edn :as edn]
     [compojure.core :refer [GET POST defroutes context]]
     [compojure.route :refer [resources]]
     [compojure.coercions :refer [as-uuid]]
-    [clostache.parser :as clostache]
     [chat.shared.util :refer [valid-nickname?]]
-    [chat.server.digest :as digest]
     [chat.server.db :as db]
     [chat.server.invite :as invites]
     [chat.server.identicons :as identicons]
@@ -21,62 +18,6 @@
 (defn- edn-response [clj-body]
   {:headers {"Content-Type" "application/edn; charset=utf-8"}
    :body (pr-str clj-body)})
-
-(defn- get-html [client]
-  (clostache/render-resource
-    (str "public/" client ".html")
-    {:algo "sha256"
-     :js (str (digest/from-file (str "public/js/" client "/out/braid.js")))
-     :api_domain (config :api-domain)}))
-
-(defroutes desktop-client-routes
-  ; public group page
-  (GET "/group/:group-name" [group-name :as req]
-    (if-let [group (db/public-group-with-name group-name)]
-      (clostache/render-resource "templates/public_group_desktop.html.mustache"
-                                 {:group-name (group :name)
-                                  :group-id (group :id)
-                                  :api-domain (config :api-domain)})
-      {:status 403
-       :headers {"Content-Type" "text/plain"}
-       :body "No such public group"}))
-
-  ; invite accept page
-  (GET "/accept" [invite :<< as-uuid tok]
-    (if (and invite tok)
-      (if-let [invite (db/get-invite invite)]
-        {:status 200 :headers {"Content-Type" "text/html"} :body (invites/register-page invite tok)}
-        {:status 400 :headers {"Content-Type" "text/plain"} :body "Invalid invite"})
-      {:status 400 :headers {"Content-Type" "text/plain"} :body "Bad invite link, sorry"}))
-
-  ; invite link
-  (GET "/invite" [group-id :<< as-uuid nonce expiry mac]
-    (if (and group-id nonce expiry mac)
-      (if (invites/verify-hmac mac (str nonce group-id expiry))
-        {:status 200 :headers {"Content-Type" "text/html"} :body (invites/link-signup-page group-id)}
-        {:status 400 :headers {"Content-Type" "text/plain"} :body "Parameter verification failed"})
-      {:status 400 :headers {"Content-Type" "text/plain"} :body "Missing required parameters"}))
-
-  ; password reset page
-  (GET "/reset" [user :<< as-uuid token :as req]
-    (if-let [u (and user token
-                 (invites/verify-reset-nonce {:id user} token)
-                 (db/user-by-id user))]
-      {:status 200
-       :headers {"Content-Type" "text/html"}
-       :body (invites/reset-page u token)}
-      {:status 401
-       :headers {"Content-Type" "text/plain"}
-       :body "Bad user or token"}))
-
-  ; everything else
-  (GET "/*" []
-    (get-html "desktop")))
-
-(defroutes mobile-client-routes
-  ; TODO: add mobile routse for public joining & password resets
-  (GET "/*" []
-    (get-html "mobile")))
 
 (defn register-user
   [email group-id]
