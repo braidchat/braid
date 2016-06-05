d7c48dc416fd3cf2e7fa5a4ba8e1a111f9cf7680
Rafal Dittwald <rafal.dittwald@gmail.com>
2016-06-05 11:07:14 -0400

    Make page background a tad lighter

--- src/braid/ui/styles/body.cljs
+++ src/braid/ui/styles/body.cljs
@@ -6,7 +6,7 @@
     :padding 0
     :font-family "\"Open Sans\", Helvetica, Arial, sans-serif"
     :font-size "12px"
-    :background "#eee"}
+    :background "#F2F2F2"}
    [:textarea :input
     {:font-family "inherit"
      :font-size "1em"}]])
