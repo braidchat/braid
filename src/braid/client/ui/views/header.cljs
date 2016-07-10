06bdeece26dd6bb0be1a56833b172d85ae236dde
James N. V. Cash <james.nvc@gmail.com>
2016-07-10 19:29:55 -0400

    use current-path subscription so headers update
    when relying on calling current-path? to determine if a button is
    active, things don't update, since page changing doesn't cause a
    re-render of those components

--- src/braid/client/ui/views/header.cljs
+++ src/braid/client/ui/views/header.cljs
@@ -5,15 +5,17 @@
             [braid.client.ui.views.search-bar :refer [search-bar-view]]))
 
 (defn inbox-page-button-view []
-  (let [open-group-id (subscribe [:open-group-id])]
+  (let [open-group-id (subscribe [:open-group-id])
+        current-path (subscribe [:page-path])]
     (fn []
       (let [path (routes/inbox-page-path {:group-id @open-group-id})]
-        [:a.inbox {:class (when (routes/current-path? path) "active")
+        [:a.inbox {:class (when (= path @current-path) "active")
                    :href path
                    :title "Inbox"}]))))
 
 (defn recent-page-button-view []
-  (let [open-group-id (subscribe [:open-group-id])]
+  (let [open-group-id (subscribe [:open-group-id])
+        current-path (subscribe [:page-path])]
     (fn []
       (let [path (routes/recent-page-path {:group-id @open-group-id})]
         [:a.recent {:class (when (routes/current-path? path) "active")
@@ -24,12 +26,13 @@
   (let [user-id (subscribe [:user-id])
         user-avatar-url (subscribe [:user-avatar-url @user-id])
         user-nickname (subscribe [:nickname @user-id])
-        open-group-id (subscribe [:open-group-id])]
+        open-group-id (subscribe [:open-group-id])
+        current-path (subscribe [:page-path])]
     (fn []
       (let [path (routes/page-path {:group-id @open-group-id
                                     :page-id "me"})]
         [:a.user-info {:href path
-                       :class (when (routes/current-path? path) "active")}
+                       :class (when (= current-path path) "active")}
           [:div.name (str "@" @user-nickname)]
           [:img.avatar {:src @user-avatar-url}]]))))
 
@@ -39,58 +42,64 @@
       [:div.group-name (@group :name)])))
 
 (defn edit-profile-link-view []
-  (let [group-id (subscribe [:open-group-id])]
+  (let [group-id (subscribe [:open-group-id])
+        current-path (subscribe [:page-path])]
     (fn []
       (let [path (routes/page-path {:group-id @group-id
                                     :page-id "me"})]
         [:a.edit-profile
-         {:class (when (routes/current-path? path) "active")
+         {:class (when (= current-path path) "active")
           :href path}
          "Edit Your Profile"]))))
 
 (defn invite-link-view []
-  (let [group-id (subscribe [:open-group-id])]
+  (let [group-id (subscribe [:open-group-id])
+        current-path (subscribe [:page-path])]
     (fn []
       (let [path (routes/invite-page-path {:group-id @group-id})]
         [:a.invite-friend
-         {:class (when (routes/current-path? path) "active")
+         {:class (when (= current-path path) "active")
           :href path}
          "Invite a Person"]))))
 
 (defn subscriptions-link-view []
-  (let [group-id (subscribe [:open-group-id])]
+  (let [group-id (subscribe [:open-group-id])
+        current-path (subscribe [:page-path])]
     (fn []
       (let [path (routes/page-path {:group-id @group-id
                                     :page-id "tags"})]
         [:a.subscriptions
-         {:class (when (routes/current-path? path) "active")
+         {:class (when (= current-path path) "active")
           :href path}
          "Manage Subscriptions"]))))
 
 (defn settings-link-view []
-  (let [group-id (subscribe [:open-group-id])]
+  (let [group-id (subscribe [:open-group-id])
+        current-path (subscribe [:page-path])]
     (fn []
       (let [path (routes/group-settings-path {:group-id @group-id})]
         [:a.settings
-         {:class (when (routes/current-path? path) "active")
+         {:class (when (= current-path path) "active")
           :href path}
          "Settings"]))))
 
 (defn group-bots-view []
-  (let [group-id (subscribe [:open-group-id])]
+  (let [group-id (subscribe [:open-group-id])
+        current-path (subscribe [:page-path])]
     (fn []
       (let [path (routes/bots-path {:group-id @group-id})]
         [:a.group-bots
-         {:class (when (routes/current-path? path) "active")
+         {:class (when (= current-path path) "active")
           :href path}
         "Bots"]))))
 
 (defn group-uploads-view []
-  (let [group-id (subscribe [:open-group-id])]
+  (let [group-id (subscribe [:open-group-id])
+        current-path (subscribe [:page-path])]
     (fn []
       (let [path (routes/uploads-path {:group-id @group-id})]
         [:a.group-uploads
-         {:class (when (routes/current-path? path) "active")
+         {:class (when (= current-path path) "active")
           :href path}
          "Uploads"]))))
 
