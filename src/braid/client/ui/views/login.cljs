d72fc3b772131fdbe32c3c469dba3dac4e5ee46f
James N. V. Cash <james.nvc@gmail.com>
2016-07-19 09:09:58 -0400

    add basic styling for github button
    Needs more work - at the very least, the alignment of the text doesn't
    match the logo

--- src/braid/client/ui/views/login.cljs
+++ src/braid/client/ui/views/login.cljs
@@ -18,59 +18,62 @@
 
     (fn [_]
       [:div.login
-       [:form {:on-submit
-               (fn [e]
-                 (.preventDefault e)
-                 (set-error! nil)
-                 (cond
-                   (not (seq (@state :email)))
-                   (set-error! "Please enter an email.")
+       [:div
+        [:form {:on-submit
+                (fn [e]
+                  (.preventDefault e)
+                  (set-error! nil)
+                  (cond
+                    (not (seq (@state :email)))
+                    (set-error! "Please enter an email.")
 
-                   (not (seq (@state :password)))
-                   (set-error! "Please enter a password.")
+                    (not (seq (@state :password)))
+                    (set-error! "Please enter a password.")
 
-                   :else
-                   (do
-                     (set-loading! true)
-                     (dispatch! :auth
-                                {:email (@state :email)
-                                 :password (@state :password)
-                                 :on-complete (fn []
-                                                (set-loading! false))
-                                 :on-error
-                                 (fn []
-                                   (set-loading! false)
-                                   (set-error! (str "Incorrect email or password. "
-                                                    " Please try again.")))}))))}
+                    :else
+                    (do
+                      (set-loading! true)
+                      (dispatch! :auth
+                                 {:email (@state :email)
+                                  :password (@state :password)
+                                  :on-complete (fn []
+                                                 (set-loading! false))
+                                  :on-error
+                                  (fn []
+                                    (set-loading! false)
+                                    (set-error! (str "Incorrect email or password. "
+                                                     " Please try again.")))}))))}
 
-        [:fieldset
-         [:label "Email"
-          [:input {:placeholder "you@example.com"
-                   :type "email"
-                   :value (@state :email)
-                   :on-change (fn [e] (set-email! (.. e -target -value)))}] ]
+         [:fieldset
+          [:label "Email"
+           [:input {:placeholder "you@example.com"
+                    :type "email"
+                    :value (@state :email)
+                    :on-change (fn [e] (set-email! (.. e -target -value)))}] ]
 
-         [:label "Password"
-          [:input {:placeholder "••••••••"
-                   :type "password"
-                   :value (@state :password)
-                   :on-change (fn [e] (set-password! (.. e -target -value)))}] ]
+          [:label "Password"
+           [:input {:placeholder "••••••••"
+                    :type "password"
+                    :value (@state :password)
+                    :on-change (fn [e] (set-password! (.. e -target -value)))}] ]
 
-         [:button.submit "Let's do this!"]
+          [:button.submit "Let's do this!"]
 
-         (when (@state :loading?)
-           [:div.spinner])
+          (when (@state :loading?)
+            [:div.spinner])
 
-         (when (@state :error)
-           [:div.error
-            [:div.message (@state :error)]
+          (when (@state :error)
+            [:div.error
+             [:div.message (@state :error)]
 
-            (when (seq (@state :email))
-              [:button.reset-password
-               {:on-click (fn [e]
-                            (.preventDefault e)
-                            (dispatch! :request-reset (@state :email)))}
-               "Send me a password reset email"])])]]
-       [:div.github
-        [:button {:on-click (fn [_] (set! (.-location js/window) "/github-login"))}
-         "Login with Github"]]])))
+             (when (seq (@state :email))
+               [:button.reset-password
+                {:on-click (fn [e]
+                             (.preventDefault e)
+                             (dispatch! :request-reset (@state :email)))}
+                "Send me a password reset email"])])]]
+        [:div.alternatives
+         "Or"
+         [:div.github
+          [:button {:on-click (fn [_] (set! (.-location js/window) "/github-login"))}
+           "Login with Github"]]]]])))
