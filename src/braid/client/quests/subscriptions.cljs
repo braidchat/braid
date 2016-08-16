db8d37914f96c42ff81bf501d8edc90143671fb7
Rafal Dittwald <rafal.dittwald@gmail.com>
2016-08-16 16:24:28 -0400

    Remove dead code

--- src/braid/client/quests/subscriptions.cljs
+++ src/braid/client/quests/subscriptions.cljs
@@ -2,10 +2,6 @@
   (:require [reagent.ratom :include-macros true :refer-macros [reaction]]
             [braid.client.state.subscription :refer [subscription]]))
 
-(defmethod subscription :quest-completed?
-  [state [_ quest-id]]
-  (reaction (contains? (get-in @state [:user :completed-quest-ids]) quest-id)))
-
 (defmethod subscription :completed-quest-count
   [state _]
   (reaction (count (get-in @state [:user :completed-quest-ids]))))
