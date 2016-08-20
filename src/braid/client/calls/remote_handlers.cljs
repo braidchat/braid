1dc0594f652afb351c254608ea6abe5f89525021
10plusY <grigsmusic1990@gmail.com>
2016-08-20 16:42:17 -0400

    Move call remote-handlers into own folder

---
+++ src/braid/client/calls/remote_handlers.cljs
@@ -0,0 +1,17 @@
+(ns braid.client.calls.remote-handlers
+  (:require
+    [braid.client.sync :as sync]
+    [braid.client.webrtc :as rtc]
+    [braid.client.dispatcher :refer [dispatch!]]))
+
+(defmethod sync/event-handler :braid.client/receive-new-call
+  [[_ call]]
+  (dispatch! :receive-new-call call))
+
+(defmethod sync/event-handler :braid.client/receive-new-call-status
+  [[_ [call status]]]
+  (dispatch! :set-receiver-call-status [call status]))
+
+(defmethod sync/event-handler :braid.client/receive-protocol-signal
+  [[_ signal]]
+  (rtc/receive-protocol-signal signal))
