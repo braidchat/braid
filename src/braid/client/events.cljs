4330f3d0f3f83689659c8ec0230219d80b6bfabd
James N. V. Cash <james.nvc@gmail.com>
2016-08-22 17:50:05 -0400

    Fix bug in adding admin, remove helper fn

--- src/braid/client/events.cljs
+++ src/braid/client/events.cljs
@@ -429,7 +429,7 @@
 (reg-event-fx
   :make-admin
   (fn [{state :db :as cofx} [_ {:keys [group-id user-id local-only?] :as args}]]
-    {:db (helpers/make-user-admin state user-id group-id)
+    {:db (update-in state [:groups group-id :admins] conj user-id)
      :websocket-send (when-not local-only?
                        (list [:braid.server/make-user-admin args]))}))
 
