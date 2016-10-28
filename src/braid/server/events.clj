909f001da860a020821cb59728808201afada692
Rafal Dittwald <rafal.dittwald@gmail.com>
2016-10-28 11:44:10 -0400

    Remove unnecessary nickname generation code
    db/create-user! already generates the nickname based on email

--- src/braid/server/events.clj
+++ src/braid/server/events.clj
@@ -1,6 +1,5 @@
 (ns braid.server.events
   (:require
-    [clojure.string :as string]
     [clavatar.core :refer [gravatar]]
     [braid.server.crypto :refer [random-nonce]]
     [braid.server.db :as db]
@@ -9,18 +8,13 @@
 (defn register-user!
   [email group-id]
   (let [id (db/uuid)
-        ; XXX: copied from braid.common.util/nickname-rd
-        disallowed-chars #"[ \t\n\]\[!\"#$%&'()*+,.:;<=>?@\^`{|}~/]"
-        nick (-> (first (string/split email #"@"))
-                 (string/replace disallowed-chars ""))
         ; TODO: guard against duplicate nickname?
         u (db/create-user! {:id id
                             :email email
                             :password (random-nonce 50)
                             :avatar (gravatar email
                                       :rating :g
-                                      :default :identicon)
-                            :nickname nick})]
+                                      :default :identicon)})]
     (sync/user-join-group! id group-id)
     id))
 
