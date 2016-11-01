a431beb551d2b1b66db1dc5fa30bb6dee38d31b3
Rafal Dittwald <rafal.dittwald@gmail.com>
2016-11-01 19:00:57 -0400

    Add missing views

---
+++ src/braid/client/register/views/join_group.cljs
@@ -0,0 +1,6 @@
+(ns braid.client.register.views.join-group)
+
+(defn join-group-view [k]
+  [:div
+   [:h2 "Join <Group Name>"]
+   ])
---
+++ src/braid/client/register/views/reset_password.cljs
@@ -0,0 +1,16 @@
+(ns braid.client.register.views.reset-password)
+
+(defn password-field-view []
+  [:div.option.password
+   [:h2 "New Password"]
+   [:label
+    [:div.field
+     [:input {:type "password"
+              :placeholder "•••••••••••"}]]]
+   [:div.explanation
+    [:p "At least 8 characters. More is better!"]]])
+
+(defn reset-password-view []
+  [:div
+   [:h2 "Reset Your Password"]
+   [password-field-view]])
