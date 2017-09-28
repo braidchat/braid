3b07e343fbd584d2217fa88401935e38d255712e
James N. V. Cash <james.nvc@gmail.com>
2017-09-28 16:07:38 -0400

    Refactor user-join-group! event to just user user-join-group-txn

--- src/braid/server/events.clj
+++ src/braid/server/events.clj
@@ -6,22 +6,12 @@
     [braid.server.db.user :as user]
     [braid.server.sync-helpers :as sync-helpers]))
 
-(defn add-user-to-recent-threads-in-group!
-  [user-id group-id]
-  (->> (thread/recent-threads {:user-id user-id
-                               :group-id group-id
-                               :num-threads 5})
-      (mapcat (fn [t] (thread/user-show-thread-txn user-id (t :id))))
-      (db/run-txns!)))
-
 (defn user-join-group!
   [user-id group-id]
-  (db/run-txns!
-    (concat
-      (group/user-add-to-group-txn user-id group-id)
-      (group/user-subscribe-to-group-tags-txn user-id group-id)))
-  (add-user-to-recent-threads-in-group! user-id group-id)
-  (sync-helpers/broadcast-group-change group-id [:braid.client/new-user (user/user-by-id user-id)]))
+  (db/run-txns! (group/user-join-group-txn user-id group-id))
+  (sync-helpers/broadcast-group-change
+    group-id
+    [:braid.client/new-user (user/user-by-id user-id)]))
 
 (defn register-user!
   [email group-id]
