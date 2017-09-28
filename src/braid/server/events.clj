df9d9fe924356154c0bcccb449a49d6d75c7fb3e
James N. V. Cash <james.nvc@gmail.com>
2017-09-28 16:00:05 -0400

    Fix running txns in user-join-group!

--- src/braid/server/events.clj
+++ src/braid/server/events.clj
@@ -8,17 +8,19 @@
 
 (defn add-user-to-recent-threads-in-group!
   [user-id group-id]
-  (doseq [t (thread/recent-threads {:user-id user-id :group-id group-id
-                                :num-threads 5})]
-    (db/run-txns! (thread/user-show-thread-txn user-id (t :id)))))
+  (->> (thread/recent-threads {:user-id user-id
+                               :group-id group-id
+                               :num-threads 5})
+      (mapcat (fn [t] (thread/user-show-thread-txn user-id (t :id))))
+      (db/run-txns!)))
 
 (defn user-join-group!
   [user-id group-id]
   (db/run-txns!
     (concat
       (group/user-add-to-group-txn user-id group-id)
-      (group/user-subscribe-to-group-tags-txn user-id group-id)
-      (add-user-to-recent-threads-in-group! user-id group-id)))
+      (group/user-subscribe-to-group-tags-txn user-id group-id)))
+  (add-user-to-recent-threads-in-group! user-id group-id)
   (sync-helpers/broadcast-group-change group-id [:braid.client/new-user (user/user-by-id user-id)]))
 
 (defn register-user!
@@ -27,4 +29,3 @@
                                                     :email email}))]
     (user-join-group! (user :id) group-id)
     (user :id)))
-
