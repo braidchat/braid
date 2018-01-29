71024c7a7b16ef5c669cb17f8632a2ba0c12b91d
James N. V. Cash <james.nvc@gmail.com>
2018-01-29 12:04:22 -0500

    Update docstrings

--- src/braid/core/module_helpers.clj
+++ src/braid/core/module_helpers.clj
@@ -1,4 +1,10 @@
 (ns braid.core.module-helpers
+  "Helpers for the Braid module system.
+  The important things here are:
+  - The `defhook` macro, which Clojure & Clojurescript should use to
+    define extension points for modules
+  - The `gen-module-requires` and `gen-module-loads` macros, which
+    `modules.cljs` uses to set up cljs module extensions"
   (:require
    [clojure.tools.reader.edn :as edn]
    [clojure.java.io :as io]))
@@ -51,7 +57,10 @@
 (defmacro gen-module-requires
   "Generate the `require` statements to pull in all necessary
   namespaces to be able to connect the modules. That is, all the
-  namespaces named in both `:extends` and `:provides`."
+  namespaces named in both `:extends` and `:provides`.
+  It seems that this should be in a separate namespace from
+  `gen-module-loads`, or the namespaces won't yet be loaded when the
+  extension calls are made."
   []
   (let [modules (read-all-modules)
         provides (->> modules
@@ -74,7 +83,9 @@
   Doing this with a macro instead of a function because we want the
   parsing of the EDN files to happen at compile time, by Clojure,
   rather than at runtime with ClojureScript (particularly because
-  ClojureScript can't actually read said module.edn files)."
+  ClojureScript can't actually read said module.edn files).
+  This should be in a namespace that requires another ns that invokes
+  the `gen-module-requires` macro."
   []
   (let [modules (read-all-modules)
         provides (->> modules
@@ -90,8 +101,8 @@
 
 (defmacro defhook
   "Macro to generate a module extension point.
-  `:writer` will be a function name that should be in the
-  `:provides` section of the module.edn.
+  `:writer` will be a function name that should be in the `:provides`
+  section of the module.edn.
   `:reader` will be deref-able and will give a collection of the
   values passed to the writer.
 
@@ -117,6 +128,10 @@
             [_#]
             (reduce
               (fn [vs# th#]
+                ;; In Clojure the module-loading will store the Vars
+                ;; themselves so we can just deref them, but in
+                ;; Clojurescript vars aren't reified at runtime, so
+                ;; module loading passes thunks
                 (~add-fn vs# (~(if clojure?
                                  `deref
                                  `(fn [x#] (x#)))
