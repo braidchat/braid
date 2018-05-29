00e1935b3c2345386de0e6e1c3ca758c7f343bf4
James N. V. Cash <james.nvc@gmail.com>
2018-05-29 13:07:16 -0400

    Add re-frame-10x to dev profile & bump a bunch of dependencies

--- project.clj
+++ project.clj
@@ -1,15 +1,16 @@
 (defproject braid "0.0.1"
   :source-paths ["src"]
 
-  :dependencies [;server
-                 [org.clojure/clojure "1.8.0"]
+  :dependencies [;;server
+                 [org.clojure/clojure "1.9.0"]
                  [javax.servlet/servlet-api "2.5"]
                  [commons-codec "1.10"]
                  [commons-validator "1.5.1"]
-                 [http-kit "2.2.0"]
-                 [ring/ring-defaults "0.2.1"]
+                 [http-kit "2.3.0"]
+                 [ring/ring-core "1.6.3"]
+                 [ring/ring-defaults "0.3.1" :exclusions [ring/ring-core]]
                  [fogus/ring-edn "0.3.0"]
-                 [ring-cors "0.1.8"]
+                 [ring-cors "0.1.12"]
                  [compojure "1.5.1"]
                  [environ "1.0.3"]
                  [com.taoensso/timbre "4.7.4" :exclusions [org.clojure/tools.reader com.taoensso/truss com.taoensso/encore]]
@@ -23,12 +24,13 @@
                  [inliner "0.1.0"]
                  [de.ubercode.clostache/clostache "1.4.0"]
                  [mount "0.1.10"]
+                 [com.fasterxml.jackson.core/jackson-core "2.8.7"]
                  [com.cognitect/transit-clj "0.8.300" :exclusions [com.fasterxml.jackson.core/jackson-core]]
                  [ring-transit "0.1.6" :exclusions [com.fasterxml.jackson.core/jackson-core]]
                  [clavatar "0.3.0"]
 
-                 ;client
-                 [org.clojure/clojurescript "1.9.671"]
+                 ;;client
+                 [org.clojure/clojurescript "1.10.238"]
                  [org.clojars.leanpixel/cljs-utils "0.4.2"]
                  [cljs-ajax "0.5.8"]
                  [secretary "1.2.3"]
@@ -36,8 +38,8 @@
                  [com.lucasbradstreet/cljs-uuid-utils "1.0.2"]
                  [com.andrewmcveigh/cljs-time "0.4.0"]
                  [clj-fuzzy "0.3.2"]
-                 [reagent  "0.6.0"]
-                 [re-frame "0.9.2" :exclusions [org.clojure/clojurescript]]
+                 [reagent  "0.7.0"]
+                 [re-frame "0.10.5" :exclusions [org.clojure/clojurescript]]
                  [cljsjs/husl "6.0.1-0"]
                  [cljsjs/highlight "9.6.0-0"]
                  [com.cognitect/transit-cljs "0.8.239"]
@@ -45,21 +47,21 @@
                  [org.clojure/core.memoize "0.5.9"]
                  [lein-doo "0.1.7"]
 
-                 ;shared
-                 [org.clojure/tools.reader "1.0.0"]
-                 [org.clojure/core.async "0.2.395" :exclusions [org.clojure/tools.reader]]
+                 ;;shared
+                 [org.clojure/tools.reader "1.3.0-alpha3"]
+                 [org.clojure/core.async "0.4.474" :exclusions [org.clojure/tools.reader]]
                  [com.taoensso/truss "1.3.6"]
                  [com.taoensso/sente "1.11.0" :exclusions [org.clojure/tools.reader taoensso.timbre com.taoensso/truss]]
                  [prismatic/schema "1.1.6"]
 
-                 ;mobile
+                 ;;mobile
                  [garden "1.3.2"]]
 
   :main braid.core.server.core
 
   :plugins [[lein-environ "1.0.0"]
             [lein-cljsbuild "1.1.6" :exclusions [org.clojure/clojure]]
-            [lein-figwheel "0.5.4-7"]
+            [lein-figwheel "0.5.16"]
             [lein-doo "0.1.7"]]
 
   :repl-options {:timeout 120000}
@@ -76,9 +78,12 @@
                 :source-paths ["src/braid"
                                "src/retouch"]
                 :compiler {:main braid.core.client.desktop.core
+                           :preloads [day8.re-frame-10x.preload]
+                           :closure-defines {"re_frame.trace.trace_enabled_QMARK_" true}
                            :asset-path "/js/dev/desktop/"
                            :output-to "resources/public/js/dev/desktop.js"
                            :output-dir "resources/public/js/dev/desktop/"
+                           :optimizations :none
                            :verbose true}}
 
                {:id "desktop-test"
@@ -135,10 +140,12 @@
                                                 com.amazonaws/aws-java-sdk
                                                 com.google.guava/guava
                                                 org.slf4j/slf4j-api]]
-                                  [figwheel-sidecar "0.5.4-7"
+                                  [figwheel-sidecar "0.5.16"
                                    :exclusions
                                    [org.clojure/google-closure-library-third-party
-                                    com.google.javascript/closure-compiler]]]}
+                                    com.google.javascript/closure-compiler
+                                    org.clojure/tools.nrepl]]
+                                  [day8.re-frame/re-frame-10x "0.3.3"]]}
              :prod {:dependencies [[com.datomic/datomic-pro "0.9.5201"
                                     :exclusions [joda-time
                                                  com.amazonaws/aws-java-sdk
