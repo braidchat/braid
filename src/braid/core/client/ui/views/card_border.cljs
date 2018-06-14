3cd67d821e201a0062113044db01b80fbc9d070b
Rafal Dittwald <rafal.dittwald@gmail.com>
2018-06-14 10:09:15 +0200

    Fix card border color

--- src/braid/core/client/ui/views/card_border.cljs
+++ src/braid/core/client/ui/views/card_border.cljs
@@ -4,10 +4,9 @@
     [braid.core.client.helpers :as helpers]))
 
 (defn card-border-view [thread-id]
-  (let [group-id @(subscribe [:open-group-id])
-        color (helpers/->color group-id)]
+  (let [color (helpers/->color @(subscribe [:open-group-id]))]
     [:div.border
      {:style
       {:background (if @(subscribe [:thread-focused? thread-id])
                      (helpers/darken color)
-                     (helpers/->color color))}}]))
+                     color)}}]))
