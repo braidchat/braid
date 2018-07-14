e13bef9edc7380680f9dee32c62b6ebb9d6982d4
James N. V. Cash <james.nvc@gmail.com>
2018-07-14 15:31:56 -0400

    Use newly-factored mixins to share styles to invite page

--- src/braid/core/client/invites/views/invite_page_styles.cljs
+++ src/braid/core/client/invites/views/invite_page_styles.cljs
@@ -7,16 +7,6 @@
 (def invite-page
   [:>.page.invite
    [:>.content
-    mixins/flex
-    {:flex-direction "column"
-     :align-items "center"}
+    (mixins/settings-container-style)
     [:>.invite
-     {:background-color "white"
-      :border-radius (px 10)
-      :padding (rem 1)
-      :font-size (rem 0.9)
-      :width "50%"}
-     [:button (mixins/settings-button)]
-     [:.invite-link
-      [:>input
-       {:width "100%"}]]]]])
+     (mixins/settings-item-style)]]])
