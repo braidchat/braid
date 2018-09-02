a649c465c1083e19675fd019fc13d27d31d09d7a
Rafal Dittwald <rafal.dittwald@gmail.com>
2018-09-02 12:04:10 -0400

    Refactor unnecessary let

--- src/braid/search/ui/search_button.cljs
+++ src/braid/search/ui/search_button.cljs
@@ -4,9 +4,8 @@
     [braid.core.client.routes :as routes]))
 
 (defn search-button-view [query]
-  (let [open-group-id (subscribe [:open-group-id])]
-    [:a.search
-     {:href (routes/group-page-path {:group-id @open-group-id
-                                     :page-id "search"
-                                     :query-params {:query query}})}
-     "Search"]))
+  [:a.search
+   {:href (routes/group-page-path {:group-id @(subscribe [:open-group-id])
+                                   :page-id "search"
+                                   :query-params {:query query}})}
+   "Search"])
