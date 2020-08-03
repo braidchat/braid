3017dd235c95a15cb84bc851a32fbacecec87bee
James N. V. Cash <james.nvc@gmail.com>
2020-08-03 14:19:10 -0400

    Update httpkit to avoid SNI errors

--- src/braid/core.clj
+++ src/braid/core.clj
@@ -2,10 +2,17 @@
   (:gen-class)
   (:require
     [mount.core :as mount]
+    [org.httpkit.client]
+    [org.httpkit.sni-client]
     [braid.core.modules :as modules]
     ;; all following requires are for mount:
     [braid.core.server.core]
     [braid.core.server.email-digest :refer [email-jobs]]))
+
+;; because we often use http-kit as our http-client
+;; including this so that SNI works
+(alter-var-root #'org.httpkit.client/*default-client*
+                (fn [_] org.httpkit.sni-client/default-client))
 
 (defn -main
   "Entry point for prod"
