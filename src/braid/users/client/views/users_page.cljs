73cd81ec1d7e959e546246464fdef3a357de44a4
James N. V. Cash <james.nvc@gmail.com>
2020-08-06 16:38:18 -0400

    Wire up "make admin" button, add "ban" button for users admin page

--- src/braid/users/client/views/users_page.cljs
+++ src/braid/users/client/views/users_page.cljs
@@ -1,16 +1,27 @@
 (ns braid.users.client.views.users-page
   (:require
-   [re-frame.core :refer [subscribe]]))
+   [re-frame.core :refer [dispatch subscribe]]))
 
 (defn user-view
-  [user]
-  (let [nickname (:nickname user)]
+  [{:keys [nickname id] :as user}]
+  (let [group-id (subscribe [:open-group-id])]
     [:tr.user
      [:td.nickname nickname]
-     [:td.action [:button.make-admin
-                  {:on-click
-                   (fn [_])}
-                  "Make Admin"]]]))
+     [:td.action
+      (if @(subscribe [:user-is-group-admin? id] [group-id])
+        [:span.admin "Admin"]
+        [:button.make-admin
+         {:on-click (fn [_]
+                      (dispatch [:make-admin
+                                 {:group-id @group-id
+                                  :user-id id}]))}
+         "Make Admin"])
+      (when (not= id @(subscribe [:user-id]))
+        [:button.ban {:on-click (fn []
+                                  (dispatch [:remove-from-group
+                                             {:group-id @group-id
+                                              :user-id id}]))}
+         "Kick from Group"])]]))
 
 (defn users-page-view []
   (when @(subscribe [:current-user-is-group-admin?]
