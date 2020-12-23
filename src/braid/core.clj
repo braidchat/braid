c35ed47f5539f631b64fe25c5f8209c557fa7ee3
Chris Hapgood <cch1@hapgood.com>
2020-12-23 09:40:36 -0500

    Provide a means for braid consumers to stop the application

--- src/braid/core.clj
+++ src/braid/core.clj
@@ -1,24 +1,34 @@
 (ns braid.core
   (:gen-class)
   (:require
-    [mount.core :as mount]
-    [org.httpkit.client]
-    [org.httpkit.sni-client]
-    [braid.core.modules :as modules]
-    ;; all following requires are for mount:
-    [braid.core.server.core]
-    [braid.core.server.email-digest :refer [email-jobs]]))
+   [mount.core :as mount]
+   [org.httpkit.client]
+   [org.httpkit.sni-client]
+   [braid.core.modules :as modules]
+   ;; all following requires are for mount:
+   [braid.core.server.core]
+   [braid.core.server.email-digest :refer [email-jobs]]))
 
 ;; because we often use http-kit as our http-client
 ;; including this so that SNI works
 (alter-var-root #'org.httpkit.client/*default-client*
                 (fn [_] org.httpkit.sni-client/default-client))
 
+(defn start!
+  "Entry point for prod"
+  [port]
+  ;; modules must run first
+  (modules/init!)
+  (-> (mount/with-args {:port port})
+      (mount/start {:port port})))
+
+(defn stop!
+  "Helper function for stopping all Braid components."
+  []
+  (mount/stop))
+
 (defn -main
   "Entry point for prod"
   [& args]
   (let [port (Integer/parseInt (first args))]
-    ; modules must run first
-    (modules/init!)
-    (-> (mount/with-args {:port port})
-        (mount/start {:port port}))))
+    (start! port)))
