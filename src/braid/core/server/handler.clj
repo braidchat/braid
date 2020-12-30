d1b20015c4b143af83d6be963954ff1ffd1e0eb9
Chris Hapgood <cch1@hapgood.com>
2020-12-30 09:56:14 -0500

    Extract session managment to bespoke meta wrapper
    Add meta wrapper for braid universal middleware (which includes
    request logging and session handling).
    
    NB: By wrapping all handlers in the universal middleware, session
    encoding/decoding now happens earlier in the middleware, as does
    general cookie processing.
    
    NB: Session handling is now present universally.  Previously it was
    present in almost every handler's enclosing middleware _except_ the
    `public-handler`.  There is no problem with attempting to decode a
    session from an anonymous user making a request to this handler and it
    has the additional benefit of allow greater insight into site usage.

--- src/braid/core/server/handler.clj
+++ src/braid/core/server/handler.clj
@@ -12,6 +12,7 @@
    [ring.middleware.defaults :refer [wrap-defaults api-defaults secure-site-defaults site-defaults]]
    [ring.middleware.format :refer [wrap-restful-format]]
    [ring.middleware.edn :refer [wrap-edn-params]]
+   [ring.middleware.session :refer [wrap-session]]
    [taoensso.timbre :as timbre]))
 
 (def session-max-age (* 60 60 24 365))
@@ -31,19 +32,6 @@
     (let [cookie-store (ns-resolve 'ring.middleware.session.cookie 'cookie-store)]
       (def session-store (cookie-store)))))
 
-(defn assoc-cookie-conf [defaults]
-  (-> defaults
-      (assoc-in [:session :cookie-name] "braid")
-      (assoc-in [:session :cookie-attrs :secure] (cond
-                                                   (env :http-only)
-                                                   false
-                                                   (= (env :environment) "prod")
-                                                   true
-                                                   :else
-                                                   false))
-      (assoc-in [:session :cookie-attrs :max-age] session-max-age)
-      (assoc-in [:session :store] session-store)))
-
 (defn assoc-csrf-conf [defaults]
   (-> defaults
       (assoc-in [:security :anti-forgery] true)))
@@ -58,6 +46,15 @@
                :content-types          true
                :default-charset        "utf-8"}})
 
+(def session-config
+  {:cookie-name "braid",
+   :cookie-attrs {:secure (cond
+                            (env :http-only) false
+                            (= (env :environment) "prod") true
+                            :else false)
+                  :max-age session-max-age},
+   :store session-store})
+
 (defn- wrap-log-requests
   [handler]
   (fn [{uri :uri method :request-method :as request}]
@@ -66,62 +63,57 @@
       (timbre/debugf "[%s +%4dms] %8.8s %s" status (- (System/currentTimeMillis) t0) method uri)
       response)))
 
-(def mobile-client-app
-  (-> (routes
-       resource-routes
-       mobile-client-routes)
-      (wrap-defaults (-> static-site-defaults
-                         assoc-cookie-conf))
+;; Here we define universal (as opposed to route-specific) middleware.
+(defn- wrap-universal-middleware
+  "Wrap the handler with middleware that is universally applicable across the site."
+  [handler options]
+  (-> handler
+      (wrap-session (options :session))
       wrap-log-requests))
 
+(def mobile-client-app
+  (-> (routes resource-routes mobile-client-routes)
+      (wrap-defaults static-site-defaults)
+      (wrap-universal-middleware {:session session-config})))
+
 (def desktop-client-app
-  (-> (routes
-       resource-routes
-       desktop-client-routes)
-      (wrap-defaults (-> static-site-defaults
-                         assoc-cookie-conf))
-      wrap-log-requests))
+  (-> (routes resource-routes desktop-client-routes)
+      (wrap-defaults static-site-defaults)
+      (wrap-universal-middleware {:session session-config})))
 
 (def api-server-app
   (-> (routes
-        modules/raw-handlers
+       modules/raw-handlers
 
-        (-> api-public-routes
-            (wrap-defaults (-> site-defaults
-                               (assoc-in [:security :anti-forgery] false)
-                               assoc-cookie-conf)))
+       (-> api-public-routes
+           (wrap-defaults (-> site-defaults
+                              (assoc-in [:security :anti-forgery] false))))
 
-        (-> modules/public-handler
-            (wrap-defaults (-> site-defaults
-                               (assoc-in [:security :anti-forgery] false)))
-            (wrap-restful-format :formats [:edn :transit-json]))
+       (-> modules/public-handler
+           (wrap-defaults (-> site-defaults
+                              (assoc-in [:security :anti-forgery] false)))
+           (wrap-restful-format :formats [:edn :transit-json]))
 
-        (-> sync-routes
-            (wrap-defaults (-> api-defaults
-                               assoc-cookie-conf
-                               assoc-csrf-conf)))
+       (-> sync-routes
+           (wrap-defaults (-> api-defaults assoc-csrf-conf)))
 
        (-> api-private-routes
-            (wrap-defaults (-> api-defaults
-                               assoc-cookie-conf
-                               assoc-csrf-conf)))
+           (wrap-defaults (-> api-defaults assoc-csrf-conf)))
 
        ;; this needs to be last, because the middleware will return
        ;; 401 if not authorized & hence not fall-through to other
        ;; routes
        (-> modules/private-handler
-           (wrap-defaults (-> site-defaults
-                              assoc-cookie-conf
-                              assoc-csrf-conf))
+           (wrap-defaults (-> site-defaults assoc-csrf-conf))
            (wrap-restful-format :formats [:edn :transit-json])))
       (wrap-cors :access-control-allow-origin (->>
-                                                [(when-let [site (env :site-url)]
-                                                   (re-pattern (java.util.regex.Pattern/quote site)))
-                                                 (when-let [mobile-site (env :mobile-site-url)]
-                                                   (re-pattern (java.util.regex.Pattern/quote mobile-site)))
-                                                 #"http://localhost:\d+"]
-                                                  (remove nil?))
+                                               [(when-let [site (env :site-url)]
+                                                  (re-pattern (java.util.regex.Pattern/quote site)))
+                                                (when-let [mobile-site (env :mobile-site-url)]
+                                                  (re-pattern (java.util.regex.Pattern/quote mobile-site)))
+                                                #"http://localhost:\d+"]
+                                               (remove nil?))
                  :access-control-allow-credentials true
                  :access-control-allow-methods [:get :put :post :delete])
       wrap-edn-params
-      wrap-log-requests))
+      (wrap-universal-middleware {:session session-config})))
