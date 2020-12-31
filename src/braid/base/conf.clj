ddfeab43948fe6b8dc9491516c6b641472cc5a02
James N. V. Cash <james.nvc@gmail.com>
2020-12-31 08:57:41 -0500

    Keep default api-domain & site-url, add explanatory comment

--- src/braid/base/conf.clj
+++ src/braid/base/conf.clj
@@ -14,8 +14,8 @@
     ;; TODO don't special case these here
     ;; ports should come from config
     {:db-url "datomic:mem://braid"
-     ;; :api-domain (str "localhost:" (+ 2 (:port (mount/args))))
-     ;; :site-url (str "http://localhost:" (:port (mount/args)))
+     :api-domain (str "localhost:" (+ 2 (:port (mount/args))))
+     :site-url (str "http://localhost:" (:port (mount/args)))
      :hmac-secret "secret"}
-    @ports-config
+    @ports-config ; overrides api-domain & site url when port is automatically found
     (select-keys env @config-vars)))
