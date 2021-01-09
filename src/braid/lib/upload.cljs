b62a9d6ed04dd81aa40f1d3b61215659b115402c
Chris Hapgood <cch1@hapgood.com>
2021-01-09 17:41:16 -0500

    Remove unused `require`s

--- src/braid/lib/upload.cljs
+++ src/braid/lib/upload.cljs
@@ -1,19 +1,18 @@
 (ns braid.lib.upload
   (:require
-    [clojure.string :as string]
-    [braid.lib.s3 :as s3]))
+   [clojure.string :as string]))
 
 (def regex (delay
              (re-pattern (str "https?://"
                               (-> (aget js/window "asset_domain")
                                   (string/replace "." "\\."))
-                                   "/"))))
+                              "/"))))
 
 (defn ->path [url]
   (string/replace
-    url
-    @regex
-    (str "//" (aget js/window "api_domain") "/upload/")))
+   url
+   @regex
+   (str "//" (aget js/window "api_domain") "/upload/")))
 
 (defn upload-path? [url]
   (string/includes? url (aget js/window "asset_domain")))
