92d31db93e650f955871fe655945cfad2d519890
James N. V. Cash <james.nvc@gmail.com>
2021-01-09 17:56:08 -0500

    Update db fixture to use new config, add missing require

--- test/braid/test/fixtures/db.clj
+++ test/braid/test/fixtures/db.clj
@@ -3,13 +3,14 @@
    [braid.core.modules :as modules]
    [braid.base.conf :as conf]
    [braid.core.server.db :as db]
+   [datomic.api]
    [mount.core :as mount]))
 
 (defn drop-db [t]
   (modules/init! modules/default)
   (-> (mount/only #{#'conf/config #'db/conn})
-      (mount/swap {#'conf/config
-                   {:db-url "datomic:mem://chat-test"}})
+      (mount/with-args {:port 0
+                        :db-url "datomic:mem://chat-test"})
       (mount/start))
   (t)
   (datomic.api/delete-database (conf/config :db-url))
