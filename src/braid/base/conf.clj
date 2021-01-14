a8f752e3785b587c7f0c64e3680c5511cfc0642b
Rafal Dittwald <rafal.dittwald@gmail.com>
2021-01-14 13:04:44 -0500

    Fix api port number after removing mobile server

--- src/braid/base/conf.clj
+++ src/braid/base/conf.clj
@@ -13,7 +13,7 @@
     ;; TODO don't special case these here
     ;; ports should come from config
     {:db-url "datomic:mem://braid"
-     :api-domain (str "localhost:" (:port (mount/args)))
+     :api-domain (str "localhost:" (inc (:port (mount/args))))
      :site-url (str "http://localhost:" (:port (mount/args)))
      :hmac-secret "secret"}
     @ports-config ; overrides api-domain & site url when port is automatically found
