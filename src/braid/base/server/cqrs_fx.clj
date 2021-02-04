d86b1a891d345ffac76dd50b17c568e6b729f094
Rafal Dittwald <rafal.dittwald@gmail.com>
2021-02-04 17:38:31 -0500

    Add namespace for common command fx

---
+++ src/braid/base/server/cqrs_fx.clj
@@ -0,0 +1,12 @@
+(ns braid.base.server.cqrs-fx
+  "Namespace that collects common CQRS command fx"
+  (:require
+    [braid.base.server.socket :as socket]
+    [braid.core.server.sync-helpers :as helpers]
+    [braid.core.server.db :as db]))
+
+(def run-txns! db/run-txns! )
+
+(def chsk-send! socket/chsk-send!)
+
+(def group-broadcast! helpers/broadcast-group-change)
