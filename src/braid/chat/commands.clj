c6d50afb8157c61f97578abc4d49a0439b93109b
Rafal Dittwald <rafal.dittwald@gmail.com>
2021-02-04 13:26:53 -0500

    Bump to latest TADA, switch command conditions to use thunks

--- src/braid/chat/commands.clj
+++ src/braid/chat/commands.clj
@@ -9,11 +9,16 @@
     :params {:thread-id uuid?
              :group-id uuid?
              :user-id uuid?}
-    :conditions (fn [{:keys [user-id thread-id group-id]}]
-                  [[(h/user-exists? (db/db) user-id) :not-found "User does not exist"]
-                   [(h/group-exists? (db/db) group-id) :not-found "Group does not exist"]
-                   [(not (h/thread-exists? (db/db) thread-id)) :forbidden "Thread already exists"]
-                   [(h/user-in-group? (db/db) user-id group-id) :forbidden "User does not belong to group"]])
+    :conditions
+    (fn [{:keys [user-id thread-id group-id]}]
+      [[#(h/user-exists? (db/db) user-id)
+        :not-found "User does not exist"]
+       [#(h/group-exists? (db/db) group-id)
+        :not-found "Group does not exist"]
+       [#(not (h/thread-exists? (db/db) thread-id))
+        :forbidden "Thread already exists"]
+       [#(h/user-in-group? (db/db) user-id group-id)
+        :forbidden "User does not belong to group"]])
     :effect
     (fn [{:keys [user-id thread-id group-id]}]
       (db/run-txns! (db.thread/create-thread-txn
--- src/braid/page_inbox/commands.clj
+++ src/braid/page_inbox/commands.clj
@@ -9,13 +9,14 @@
   [{:id :braid.inbox/hide-thread!
     :params {:thread-id uuid?
              :user-id uuid?}
-    :conditions (fn [{:keys [user-id thread-id]}]
-                  [[(h/user-exists? (db/db) user-id)
-                    :not-found "User does not exist"]
-                   [(h/thread-exists? (db/db) thread-id)
-                    :not-found "Thread does not exist"]
-                   [(h/user-has-thread-open? (db/db) user-id thread-id)
-                    :forbidden "User does not have thread open"]])
+    :conditions
+    (fn [{:keys [user-id thread-id]}]
+      [[#(h/user-exists? (db/db) user-id)
+        :not-found "User does not exist"]
+       [#(h/thread-exists? (db/db) thread-id)
+        :not-found "Thread does not exist"]
+       [#(h/user-has-thread-open? (db/db) user-id thread-id)
+        :forbidden "User does not have thread open"]])
     :effect
     (fn [{:keys [user-id thread-id]}]
       (db/run-txns!
@@ -25,15 +26,16 @@
    {:id :braid.inbox/show-thread!
     :params {:thread-id uuid?
              :user-id uuid?}
-    :conditions (fn [{:keys [user-id thread-id]}]
-                  [[(h/user-exists? (db/db) user-id)
-                    :not-found "User does not exist"]
-                   [(h/thread-exists? (db/db) thread-id)
-                    :not-found "Thread does not exist"]
-                   [(not (h/user-has-thread-open? (db/db) user-id thread-id))
-                    :forbidden "User already has thread open"]
-                   [(h/user-can-access-thread? (db/db) user-id thread-id)
-                    :forbidden "User cannot access this thread"]])
+    :conditions
+    (fn [{:keys [user-id thread-id]}]
+      [[#(h/user-exists? (db/db) user-id)
+        :not-found "User does not exist"]
+       [#(h/thread-exists? (db/db) thread-id)
+        :not-found "Thread does not exist"]
+       [#(not (h/user-has-thread-open? (db/db) user-id thread-id))
+        :forbidden "User already has thread open"]
+       [#(h/user-can-access-thread? (db/db) user-id thread-id)
+        :forbidden "User cannot access this thread"]])
     :effect
     (fn [{:keys [user-id thread-id]}]
       (db/run-txns! (db.thread/user-show-thread-txn user-id thread-id))
