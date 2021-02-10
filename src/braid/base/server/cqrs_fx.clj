f90310b673b548309edd524e58be4c94e6cf2e64
Rafal Dittwald <rafal.dittwald@gmail.com>
2021-02-10 13:15:54 -0500

    [base] Add cqrs/dispatch!

--- src/braid/base/server/cqrs_fx.clj
+++ src/braid/base/server/cqrs_fx.clj
@@ -3,10 +3,13 @@
   (:require
     [braid.base.server.socket :as socket]
     [braid.core.server.sync-helpers :as helpers]
-    [braid.core.server.db :as db]))
+    [braid.core.server.db :as db]
+    [braid.base.server.cqrs :as cqrs]))
 
 (def run-txns! db/run-txns!)
 
 (def chsk-send! socket/chsk-send!)
 
 (def group-broadcast! helpers/broadcast-group-change)
+
+(def dispatch! cqrs/dispatch)
