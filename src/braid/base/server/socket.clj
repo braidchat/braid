38f41066fb83e15185424a52be587db92ef5e2a6
Rafal Dittwald <rafal.dittwald@gmail.com>
2021-04-26 11:58:24 -0400

    [base] Enable sente simple-auto-threading
    Long-running socket-message handlers can hold up the sente websocket go loop causing timeouts for all clients.
    
    See: https://github.com/ptaoussanis/sente/issues/265#issuecomment-247524010

--- src/braid/base/server/socket.clj
+++ src/braid/base/server/socket.clj
@@ -12,6 +12,7 @@
                                   {:user-id-fn
                                    (fn [ob] (or (get-in ob [:session :user-id])
                                                (get-in ob [:session :fake-id])))
+                                   :simple-auto-threading? true
                                    :csrf-token-fn nil
                                    :ws-kalive-ms 30000
                                    :packer packer})]
